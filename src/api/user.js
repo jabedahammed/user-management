85d2ee98acbf016bdd552c61437565ed5f484f20
jabed <jabedahammed90@gmail.com>
2022-10-13 19:40:51 +0530

    change user api

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,12 @@ const saveUser= async (data)=>{
     let user =  await post("user/save-user",data);
     return user;
 }
+
+
+const getuser = async ()=>{
+    return "hello"
+}
+
 export {
-    saveUser
+    saveUser,getuser
 }
